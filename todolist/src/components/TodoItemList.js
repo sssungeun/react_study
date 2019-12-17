44b27b40b0d4a31119c2cfe5a8247974ab5e1269
sssungeun <51014797+sssungeun@users.noreply.github.com>
2019-12-18 00:20:26 +0900

    Add files via upload

---
+++ todolist/src/components/TodoItemList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
+
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
+
+    const todoList = todos.map( //map은 배열형태?
+      (todo) => (
+        <TodoItem
+          {...todo}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          key={todo.id}
+        />
+      )
+    );
+
+    return (
+      <div>
+        {todoList}    
+      </div>
+    );
+  }
+}
+
+export default TodoItemList;
